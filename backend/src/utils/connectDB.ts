db84c01be55f582709d221a5e368624197483269
memberx <akhvlediani.giorgi@gmail.com>
2023-01-11 20:45:39 +0400

    fix

--- backend/src/utils/connectDB.ts
+++ backend/src/utils/connectDB.ts
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose';
 import config from 'config';
 
+/* 
 const dbUrl = `mongodb://${config.get('dbName')}:${config.get(
   'dbPass'
 )}@localhost:6000/jwtAuth?authSource=admin`;
+*/
+
+const dbUrl = `mongodb+srv://mongo:1234@cluster0.607nzx6.mongodb.net/test`;
 
 const connectDB = async () => {
   try {
